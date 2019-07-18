32347498e58c6483e8899a6c0bd2beca7d6f3da3
NYHolzer <nissan.holzer@gmail.com>
2019-07-17 21:28:53 -0400

    add presence validation & zip val. building us_states helper

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,5 +2,6 @@ class Building < ApplicationRecord
     has_many :units
     has_many :users, through: :units
 
-    validates :
+    validates :name, :address1, :city, :state, :zip_code, presence: true
+    validates :zip_code, numericality: true
 end
