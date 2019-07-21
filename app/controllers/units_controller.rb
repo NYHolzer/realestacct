02710fef16d284394e44762fbfffe319b4e65fd1
NYHolzer <nissan.holzer@gmail.com>
2019-07-21 11:09:34 -0400

    fixed new unit button from buildings show page

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -6,7 +6,7 @@ class UnitsController < ApplicationController
     end
 
     def new 
-        if params[:building_id] && !Building.exists?(params[:buidling_id])
+        if params[:building_id] && !Building.exists?(params[:building_id])
             redirect_to buildings_path, alert: "Building not found!"
         else 
             @unit = Unit.new(building_id: params[:building_id])
@@ -16,7 +16,7 @@ class UnitsController < ApplicationController
     def create
         @unit = Unit.new(unit_params)
         @unit.user_id = current_user.id
-        if @unit.save && @unit.building-_state.blank?
+        if @unit.save && @unit.building_state.blank?
             redirect_to edit_building_path(@unit.building)
         elsif @unit.save
             redirect_to building_path(@unit.building)
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,7 +1,7 @@
 class Unit < ApplicationRecord
     belongs_to :user
     belongs_to :building
-    
+
     def building_name=(name)
         self.building = Building.find_or_create_by(name: name)
     end
