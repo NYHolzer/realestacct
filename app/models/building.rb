dc4c0baffa40b5b14e2e0d612904caac406ee377
NYHolzer <nissan.holzer@gmail.com>
2019-07-21 01:03:15 -0400

    Fixed bug validation so edit_building_path works when creating new unit

--- app/models/building.rb
+++ app/models/building.rb
@@ -3,9 +3,9 @@ class Building < ApplicationRecord
     has_many :users, through: :units
 
     validates :name, presence: true
-    validates :name, uniqueness: true
-    validates :city, format: {with: /\A[a-zA-Z\s]*\z/, message: "must only contain letters"}
-    validates :state, length: {is: 2}
-    validates :zip_code, numericality: true
-    validates :zip_code, length: {is: 5}
+    validates :name, uniqueness: true, on: :update
+    validates :city, format: {with: /\A[a-zA-Z\s]*\z/, message: "must only contain letters"}, on: :update
+    validates :state, length: {is: 2}, on: :update
+    validates :zip_code, numericality: true, on: :update
+    validates :zip_code, length: {is: 5}, on: :update
 end
