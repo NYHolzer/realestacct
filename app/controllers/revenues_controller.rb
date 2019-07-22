377abd03240bd19f94ba4e1c21e44badf669f732
NYHolzer <nissan.holzer@gmail.com>
2019-07-21 22:17:26 -0400

    successfully added revenue to unit

--- app/controllers/revenues_controller.rb
+++ app/controllers/revenues_controller.rb
@@ -10,10 +10,15 @@ class RevenuesController < ApplicationController
 
     def create
         @revenue = Revenue.new(revenue_params)
-        if @unit.save
-            redirect_to unit_path(@unit)
+        if @revenue.save
+            redirect_to unit_path(@revenue.unit)
         else 
             render :new
         end
+    end
+
+    private 
+    def revenue_params
+        params.require(:revenue).permit(:rev_type, :amount, :date, :unit_id)
     end
 end
