e040d6704c9363e7a3c1dec5fb42ae58165a4086
NYHolzer <nissan.holzer@gmail.com>
2019-09-16 21:05:10 -0400

    Added apps/javascript and added application.js and buildings.js file with some code

---
+++ app/javascripts/buildings.js
@@ -0,0 +1,9 @@
+// Want to start listening only after the page has loaded
+$(function() {
+    console.log('building.js is loaded...')
+    listenForClick()
+})
+
+function listenForClick() {
+    $('button')
+}
