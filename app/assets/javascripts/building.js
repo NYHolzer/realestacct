213db8b49534c8aecc49bc8b75ed839e8a565d8b
NYHolzer <nissan.holzer@gmail.com>
2019-09-17 22:37:00 -0400

    working on getting build data into a JS class

--- app/assets/javascripts/building.js
+++ app/assets/javascripts/building.js
@@ -1,5 +1,5 @@
 
-function buildingButtonClick() {
+function buildingButtonClick(){
     document.querySelector('.buildings').addEventListener("click", function(e){
         e.preventDefault
         requestBuildings()
@@ -9,5 +9,28 @@ function buildingButtonClick() {
 function requestBuildings(){
     fetch('http://localhost:3000/buildings.json')
         .then((resp) => resp.json())
-        .then((data) => console.log(data))
+        .then((data) => {
+            let buildingsData = data
+            getBuildingsData(buildingsData)
+        })
+        
+    }
+
+function getBuildingsData(buildingsData) {
+    debugger
+    buildingsData.forEach(new Building(obj))
+    console.log(Building)
+}
+
+class Building {
+    constructor(obj) {
+        this.id = obj.id
+        this.name = obj.name
+        this.address1 = obj.address1
+        this.address2 = obj.address2
+        this.city = obj.city
+        this.state = obj.state 
+        this.zip_code = obj.zip_code
+        this.units = obj.units
+    }
 }
