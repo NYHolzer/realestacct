0dc03e7da7dff8d1ac0466f0f029d3837b9c83f7
NYHolzer <nissan.holzer@gmail.com>
2019-09-17 21:39:45 -0400

    added building button to header and added JS listener to button

--- app/assets/javascripts/building.js
+++ app/assets/javascripts/building.js
@@ -1,17 +1,6 @@
-// // Want to start listening only after the page has loaded
-// $(function() {
-//     console.log('building.js is loaded...')
-//     listenForClick()
-// })
 
-function listenForClick() {
-    document.querySelector('.test').addEventListener("click", function(e){
+function buildingButtonClick() {
+    document.querySelector('.buildings').addEventListener("click", function(e){
         console.log(this)
     })
 }
-
-// function listenForClick() {
-//     $('.test').on('click', function(){
-//         console.log("Test This is Working YAY!!")
-//     })
-// }
