5f8fef753d5df0a7e73e21a5161b0053512f960d
NYHolzer <nissan.holzer@gmail.com>
2019-09-25 21:09:32 -0400

    Added button html to indicate clickable for more information

--- app/assets/javascripts/building.js
+++ app/assets/javascripts/building.js
@@ -23,7 +23,7 @@ function setBuildings(buildingsData) {
 
     buildingsData.forEach(function(building){
         let b = new Building(building)
-        buildingsList.innerHTML += `<li id=${b.id} onclick=buildingDetailButtonClick()>${b.name}</li>`
+        buildingsList.innerHTML += `<li id=${b.id} onclick=buildingDetailButtonClick()><button>${b.name}</button></li>`
     })  
 
     newMain.appendChild(buildingsList)
